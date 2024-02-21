5ddefc9ed4c035e983505d716f53335579e648dd
Kiruthika-01 <124856724+Kiruthika-01@users.noreply.github.com>
2024-02-21 10:58:28 +0530

    Add files via upload

---
+++ review/review/src/main/java/com/bookreview/review/repository/ReviewRepository.java
@@ -0,0 +1,19 @@
+// package com.bookreview.review.repository;
+
+// public class ReviewRepository {
+    
+// }
+
+
+package com.bookreview.review.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bookreview.review.model.ReviewDetails;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<ReviewDetails,Integer>{
+
+    
+}
